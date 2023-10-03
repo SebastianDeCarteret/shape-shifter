921d15b5754bc57ec66bc215344dccee6309b8c7
Seb <sebastian.decarteret@barnett-waddingham.co.uk>
2023-10-03 16:53:51 +0100

    actaully didn't solve the overlap

--- challenges/hard/rectangleOverlap.js
+++ challenges/hard/rectangleOverlap.js
@@ -16,10 +16,43 @@ const rectangleAreaFromCoords = require("../medium/rectangleAreaFromCoords.js");
  * @param {{x: number, y: number}[]} rectangle1 - The four corners of rectangle 1
  * @param {{x: number, y: number}[]} rectangle2 - The four corners of rectangle 2
  */
+// function rectangleOverlap(rectangle1, rectangle2) {
+//   return rectangleAreaFromCoords(rectangle1) != 0 ||
+//     rectangleAreaFromCoords(rectangle2) != 0
+//     ? rectangleAreaFromCoords(rectangle1) / rectangleAreaFromCoords(rectangle2)
+//     : 0;
+// }
+
 function rectangleOverlap(rectangle1, rectangle2) {
-  return (
-    rectangleAreaFromCoords(rectangle1) / rectangleAreaFromCoords(rectangle2)
+  [A1, B1, C1, D1] = rectangle1;
+  [A2, B2, C2, D2] = rectangle2;
+
+  rect1 = {
+    left: Math.abs(D1.y - A1.y),
+    right: Math.abs(B1.y - C1.y),
+    top: Math.abs(C1.x - D1.x),
+    bottom: Math.abs(A1.x - B1.x),
+  };
+
+  rect2 = {
+    left: Math.abs(D2.y - A2.y),
+    right: Math.abs(B2.y - C2.y),
+    top: Math.abs(C2.x - D2.x),
+    bottom: Math.abs(A2.x - B2.x),
+  };
+
+  console.log("rect1", rect1);
+  console.log("rect2", rect2);
+
+  x_overlap = Math.max(
+    0,
+    Math.min(rect1.right, rect2.right) - Math.max(rect1.left, rect2.left)
   );
+  y_overlap = Math.max(
+    0,
+    Math.min(rect1.bottom, rect2.bottom) - Math.max(rect1.top, rect2.top)
+  );
+  return x_overlap * y_overlap;
 }
 
 module.exports = rectangleOverlap;
--- challenges/medium/rectangleAreaFromCoords.js
+++ challenges/medium/rectangleAreaFromCoords.js
@@ -5,7 +5,6 @@
  * @param {{ x: number, y: number }[]} points - an array of 4 points, e.g. { x: 3, y: 5 }
  * @return {number} the area
  */
-
 function rectangleAreaFromCoords(points) {
   [A, B, C, D] = points;
   // weird formula I made up:
