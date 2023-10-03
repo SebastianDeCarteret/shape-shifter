7abccb4c7c884a0fd01e8d2126e1b13909d20718
Seb <sebastian.decarteret@barnett-waddingham.co.uk>
2023-10-03 12:36:46 +0100

    completed 1 hard challenge and  started another

--- challenges/hard/triangleAreaFromCoords.js
+++ challenges/hard/triangleAreaFromCoords.js
@@ -3,12 +3,21 @@
  * The triangle is not assumed to be a right-triangle. The points are given in an array, e.g.
  * [{ x: 3, y: 4}, { x: 4, y: 6 }, { x: 7, y: 1 }]
  * @param {{ x: number, y: number }[]} points - The three vertices of the triangle
- * @returns 
+ * @returns
  */
+let myString = "test";
+myString;
 
-function triangleAreaFromCoords (points) {
-  [A, B, C] = points
-  return 0.5 * ((A.x * B.x + B.x * C.x + C.x * A.x) - (A.y * B.y + B.y * C.y + C.y * A.y))
+function triangleAreaFromCoords(points) {
+  [A, B, C] = points;
+  // return (
+  //   0.5 *
+  //   (A.x * B.x + B.x * C.x + C.x * A.x - (A.y * B.y + B.y * C.y + C.y * A.y))
+  // );
+  return (
+    0.5 * (A.x * B.y + B.x * C.y + C.x * A.y) -
+    (A.y * B.x + B.y * C.x + C.y * A.x)
+  );
 }
 
-module.exports = triangleAreaFromCoords
+module.exports = triangleAreaFromCoords;
--- challenges/hard/triangleAreaFromSides.js
+++ challenges/hard/triangleAreaFromSides.js
@@ -1,18 +1,20 @@
-const trianglePerimeter = require('../easy/trianglePerimeter')
+const trianglePerimeter = require("../easy/trianglePerimeter.js");
 
 /**
  * Uses Heron's formula (https://en.wikipedia.org/wiki/Heron%27s_formula) to give the area of a triangle
  * with sides a,b,c. The triangle isn't assumed to be a right-triangle.
- * @param {number} a 
- * @param {number} b 
- * @param {number} c 
+ * @param {number} a - side a
+ * @param {number} b - side b
+ * @param {number} c - side c
  * @returns {number} the area
-*/
+ */
 
-function triangleAreaFromSides (a, b, c) {
-  const s = trianglePerimeter(a, b, c) / 2
-  // I'm going to lunch, I'll do the rest later... probably
-  return
+function triangleAreaFromSides(a, b, c) {
+  const perimeter = trianglePerimeter(a, b, c) / 2;
+  const area = Math.sqrt(
+    (perimeter - a) * (perimeter - b) * (perimeter - c) * perimeter
+  );
+  return area;
 }
 
-module.exports = triangleAreaFromSides
+module.exports = triangleAreaFromSides;
